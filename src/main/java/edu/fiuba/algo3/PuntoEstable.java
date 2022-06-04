2325e1e7563c757b35f4260ee47d3cb53602d059
theojorge <tjorge@fi.uba.ar>
2022-06-04 20:53:56 -0300

    Revert "Inicio grupal"
    This reverts commit ad60e592fa7bfabd15915b5d98ff2e573228809c.

--- src/main/java/edu/fiuba/algo3/PuntoEstable.java
+++
@@ -1,7 +0,0 @@
-package edu.fiuba.algo3;
-
-public interface PuntoEstable {
-    void agregarCuadra(Cuadra cuadra, int i) ;
-    boolean validarSentido(Sentido sentido);
-    PuntoEstable siguienteCruce(Sentido sentido, Vehiculo vehiculo);
-}
--- src/main/java/edu/fiuba/algo3/Vehiculo.java
+++
@@ -1,6 +0,0 @@
-package edu.fiuba.algo3;
-
-public abstract class Vehiculo {
-    private int movimientos;
-    public void interactuar(Elemento elemento){}
-}
