3078fd4e2c508502c921e6d25f7899a2a3e04dd5
hstrangis <nstrangis@fi.uba.ar>
2022-07-16 14:33:35 -0300

    Agrego test de jugador y vehiculo

---
+++ src/main/test/edu/fiuba/algo3/JugadorTest.java
@@ -0,0 +1,28 @@
+package edu.fiuba.algo3;
+
+import edu.fiuba.algo3.modelo.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JugadorTest {
+    @Test
+    public void testMuevoJugadorYVerificoQueCambiaSuUbicacion() {
+        Vehiculo vehiculo = new Vehiculo(new Moto(), new Movimientos());
+        Juego juego = new Juego(4,4);
+        juego.jugar("carlos", vehiculo, new Meta());
+        PuntoEstable UbicacionVieja = juego.jugador().ubicacion();
+        juego.moverse(new Este());
+        assertNotEquals(UbicacionVieja, juego.jugador().ubicacion());
+    }
+    @Test
+    public void testJugadorSeMueve3VecesYNoEncuentraObstaculos() {
+        Vehiculo vehiculo = new Vehiculo(new Moto(), new Movimientos());
+        Juego juego = new Juego(4,4);
+        juego.jugar("carlos", vehiculo, new Meta());
+        juego.moverse(new Este());
+        juego.moverse(new Este());
+        juego.moverse(new Este());
+        assertEquals(3, juego.jugador().movimientos().cantidad());
+    }
+}
